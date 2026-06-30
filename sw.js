fe1569e3e1eec3efed12fd46097f924f9c39793d
OVA <479572309@qq.com>
2026-06-30 11:10:19 +0800

    update daily readings

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "ova-yanxishe-pwa-v20260630-officer4";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css?v=20260630-officer4",
+  "./script.js?v=20260630-officer4",
+  "./manifest.webmanifest",
+  "./enso_circle_print_thin.png",
+  "./oriental_woman_bg.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+        return null;
+      })))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => {
+      return caches.match(event.request).then((cached) => {
+        if (cached) return cached;
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
